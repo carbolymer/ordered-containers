f03e41d5c6f9c8141d2ae9d14c901fbd564d7da6
Mateusz Galazyn <carbolymer@gmail.com>
2022-08-19 20:16:17 +0200

    added test

--- test/Data/MultiMap/Ordered/StrictSpec.hs
+++ test/Data/MultiMap/Ordered/StrictSpec.hs
@@ -1,9 +1,28 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Data.MultiMap.Ordered.StrictSpec where
 
 import Test.Hspec
+import qualified Data.Map.Ordered as MOM
+import qualified Data.MultiMap.Ordered.Strict as OM
 
 spec :: Spec
 spec = do
-  describe "foo" $ do
-    it "foo foo" $ do
-      True == False
+  describe "inserting elements" $ do
+    it "one by one" $ do
+      let theMap = foldr (uncurry OM.insert) mempty [(1, 3), (1, 2), (1, 1), (3, 3), (3, 2), (2, 1), (2, 3), (2, 2), (3, 1)]
+      theMap == MOM.fromList @Int @[Int] [(3, [1, 2, 3]), (2, [2, 3, 1]), (1, [1, 2, 3])]
+
+  it "whole list" $ do
+    let theMap = OM.insert 1 3 $ OM.insert 1 2 $ OM.insert 1 1
+          $ OM.insertAll 3 [2, 3] $ OM.insertAll 2 [2, 3] $ OM.insert 3 1 $ OM.insert 2 1 mempty
+    theMap == MOM.fromList @Int @[Int] [(2, [1, 2, 3]), (3, [1, 2, 3]), (1, [1, 2, 3])]
+
+  describe "conversion" $ do
+    it "from list" $ do
+      let theMap = OM.fromList [(2, 1), (3, 1), (2, 2), (1, 1), (2, 3), (1, 2), (3, 2), (3, 3), (1, 3)]
+      theMap == MOM.fromList @Int @[Int] [(2, [1, 2, 3]), (3, [1, 2, 3]), (1, [1, 2, 3])]
+
+    it "to list" $ do
+      let theList = OM.toList $ MOM.fromList @Int @[Int] [(2, [1, 2, 3]), (3, [1, 2, 3]), (1, [1, 2, 3])]
+      theList == [(2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3), (1, 1), (1, 2), (1, 3)]
